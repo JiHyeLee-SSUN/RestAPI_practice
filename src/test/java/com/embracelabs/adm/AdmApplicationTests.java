b06214981404e6551f4a02b0d13a80d17c88bfe2
leejihye <altjd815@gmail.com>
2020-11-01 14:02:00 +0900

    ### first commit ###

---
+++ src/test/java/com/embracelabs/adm/AdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.embracelabs.adm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
